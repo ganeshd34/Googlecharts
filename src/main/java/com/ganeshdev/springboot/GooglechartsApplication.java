51768dad1345071233eb4167aeb64680adcbbd3a
Ganesh Devadiga <ganesh.devadiga2@iqvia.com>
2022-04-30 17:58:03 +0530

    first commit

---
+++ src/main/java/com/ganeshdev/springboot/GooglechartsApplication.java
@@ -0,0 +1,13 @@
+package com.ganeshdev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglechartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglechartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganeshdev/googlecharts/GooglechartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshdev.googlecharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglechartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
